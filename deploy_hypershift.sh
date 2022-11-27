6371d697f90d4e667db92af507748a98239f6494
Liangquan Li <liangli@redhat.com>
2022-11-27 13:11:47 +0800

    update deploy_hypershift.sh

--- deploy_hypershift.sh
+++ deploy_hypershift.sh
@@ -9,6 +9,14 @@ playbooks_dir=${__dir}/playbooks
 export SSH_PUB_KEY=$(cat /root/.ssh/id_rsa.pub)
 export HOSTED_CLUSTER_NAME=$(oc get hostedclusters -n clusters -ojsonpath="{.items[0].metadata.name}")
 export HOSTED_CONTROL_PLANE_NAMESPACE="clusters-"${HOSTED_CLUSTER_NAME}
+export ASSISTED_PULLSECRET_NAME="${ASSISTED_PULLSECRET_NAME:-assisted-pull-secret}"
+export ASSISTED_PULLSECRET_JSON="${ASSISTED_PULLSECRET_JSON:-${PULL_SECRET_FILE}}"
+export ASSISTED_PRIVATEKEY_NAME="${ASSISTED_PRIVATEKEY_NAME:-assisted-ssh-private-key}"
+
+oc get secret "${ASSISTED_PULLSECRET_NAME}" -n "${HOSTED_CONTROL_PLANE_NAMESPACE}" || \
+    oc create secret generic "${ASSISTED_PULLSECRET_NAME}" --from-file=.dockerconfigjson="${ASSISTED_PULLSECRET_JSON}" --type=kubernetes.io/dockerconfigjson -n "${HOSTED_CONTROL_PLANE_NAMESPACE}"
+oc get secret "${ASSISTED_PRIVATEKEY_NAME}" -n "${HOSTED_CONTROL_PLANE_NAMESPACE}" || \
+    oc create secret generic "${ASSISTED_PRIVATEKEY_NAME}" --from-file=ssh-privatekey=/root/.ssh/id_rsa --type=kubernetes.io/ssh-auth -n "${HOSTED_CONTROL_PLANE_NAMESPACE}"
 
 envsubst <<"EOF" | oc apply -f -
 apiVersion: agent-install.openshift.io/v1beta1
@@ -18,7 +26,7 @@ metadata:
   namespace: ${HOSTED_CONTROL_PLANE_NAMESPACE}
 spec:
   pullSecretRef:
-    name: pull-secret
+    name: ${ASSISTED_PULLSECRET_NAME}
   sshAuthorizedKey: ${SSH_PUB_KEY}
 EOF
 
