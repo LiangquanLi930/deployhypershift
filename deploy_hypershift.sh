deec7b3a4e5eca0274f21703ff30b2616d08ca6b
Liangquan Li <liangli@redhat.com>
2022-11-28 13:33:57 +0800

    add infraenv

--- deploy_hypershift.sh
+++ deploy_hypershift.sh
@@ -9,22 +9,22 @@ playbooks_dir=${__dir}/playbooks
 export SSH_PUB_KEY=$(cat /root/.ssh/id_rsa.pub)
 export HOSTED_CLUSTER_NAME=$(oc get hostedclusters -n clusters -ojsonpath="{.items[0].metadata.name}")
 export HOSTED_CONTROL_PLANE_NAMESPACE="clusters-"${HOSTED_CLUSTER_NAME}
+export ASSISTED_PULLSECRET_JSON="${ASSISTED_PULLSECRET_JSON:-${PULL_SECRET_FILE}}"
+export INFRAENV_NAME=${HOSTED_CLUSTER_NAME}
 
-envsubst <<"EOF" | oc apply -f -
-apiVersion: agent-install.openshift.io/v1beta1
-kind: InfraEnv
-metadata:
-  name: ${HOSTED_CLUSTER_NAME}
-  namespace: ${HOSTED_CONTROL_PLANE_NAMESPACE}
-spec:
-  pullSecretRef:
-    name: pull-secret
-  sshAuthorizedKey: ${SSH_PUB_KEY}
-EOF
+echo "Running Ansible playbook to create kubernetes objects"
+ansible-playbook "${__dir}/assisted-installer-crds-playbook.yaml"
+
+oc get secret pull-secret -n "${HOSTED_CONTROL_PLANE_NAMESPACE}" || \
+    oc create secret generic pull-secret --from-file=.dockerconfigjson="${ASSISTED_PULLSECRET_JSON}" --type=kubernetes.io/dockerconfigjson -n "${HOSTED_CONTROL_PLANE_NAMESPACE}"
+oc get secret "${ASSISTED_PRIVATEKEY_NAME}" -n "${HOSTED_CONTROL_PLANE_NAMESPACE}" || \
+    oc create secret generic "${ASSISTED_PRIVATEKEY_NAME}" --from-file=ssh-privatekey=/root/.ssh/id_rsa --type=kubernetes.io/ssh-auth -n "${HOSTED_CONTROL_PLANE_NAMESPACE}"
+
+for manifest in $(find ${__dir}/generated -type f); do
+    tee < "${manifest}" >(oc apply -f -)
+done
+
+#oc wait --timeout=5m --for=condition=ImageCreated --namespace=${HOSTED_CONTROL_PLANE_NAMESPACE} InfraEnv/${INFRAENV_NAME}
 
 #wait_for_condition "infraenv/${HOSTED_CLUSTER_NAME}" "ImageCreated" "5m" "${HOSTED_CONTROL_PLANE_NAMESPACE}"
 #export ISO_DOWNLOAD_URL=$(oc get -n $HOSTED_CONTROL_PLANE_NAMESPACE infraenv $HOSTED_CLUSTER_NAME -o jsonpath='{.status.isoDownloadURL}')
-
-#echo "Apply BareMetalHost on hub"
-#ansible-playbook "${playbooks_dir}/bmh-playbook.yaml"
-#oc apply -f ${playbooks_dir}/generated/baremetalHost.yaml -n $HOSTED_CONTROL_PLANE_NAMESPACE
