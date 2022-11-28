819a78d551acb8e5865e085c08f009993c6b41fe
Liangquan Li <liangli@redhat.com>
2022-11-28 15:27:19 +0800

    update deploy_hypershift.sh

--- deploy_hypershift.sh
+++ deploy_hypershift.sh
@@ -13,14 +13,14 @@ export ASSISTED_PULLSECRET_JSON="${ASSISTED_PULLSECRET_JSON:-${PULL_SECRET_FILE}
 export INFRAENV_NAME=${HOSTED_CLUSTER_NAME}
 
 echo "Running Ansible playbook to create kubernetes objects"
-ansible-playbook "${__dir}/bmh-playbook.yaml"
+ansible-playbook "${playbooks_dir}/bmh-playbook.yaml"
 
 oc get secret pull-secret -n "${HOSTED_CONTROL_PLANE_NAMESPACE}" || \
     oc create secret generic pull-secret --from-file=.dockerconfigjson="${ASSISTED_PULLSECRET_JSON}" --type=kubernetes.io/dockerconfigjson -n "${HOSTED_CONTROL_PLANE_NAMESPACE}"
 oc get secret "${ASSISTED_PRIVATEKEY_NAME}" -n "${HOSTED_CONTROL_PLANE_NAMESPACE}" || \
     oc create secret generic "${ASSISTED_PRIVATEKEY_NAME}" --from-file=ssh-privatekey=/root/.ssh/id_rsa --type=kubernetes.io/ssh-auth -n "${HOSTED_CONTROL_PLANE_NAMESPACE}"
 
-for manifest in $(find ${__dir}/generated -type f); do
+for manifest in $(find ${playbooks_dir}/generated -type f); do
     tee < "${manifest}" >(oc apply -f -)
 done
 
