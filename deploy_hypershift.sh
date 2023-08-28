c01b1f93c4353dab4e06c6a8f393b81c78faf467
Liangquan Li <liangli@redhat.com>
2023-08-28 10:27:59 +0800

    update deploy_hypershift.sh

--- deploy_hypershift.sh
+++ deploy_hypershift.sh
@@ -7,8 +7,9 @@ source /root/dev-scripts-additional-config
 playbooks_dir=${__dir}/playbooks
 
 export SSH_PUB_KEY=$(cat /root/.ssh/id_rsa.pub)
-export HOSTED_CLUSTER_NAME=$(oc get hostedclusters -n clusters -ojsonpath="{.items[0].metadata.name}")
-export HOSTED_CONTROL_PLANE_NAMESPACE="clusters-"${HOSTED_CLUSTER_NAME}
+export HOSTED_CLUSTER_NS=$(oc get hostedcluster -A -ojsonpath='{.items[0].metadata.namespace}')
+export HOSTED_CLUSTER_NAME=$(oc get hostedclusters -n "$HOSTED_CLUSTER_NS" -ojsonpath="{.items[0].metadata.name}")
+export HOSTED_CONTROL_PLANE_NAMESPACE=${HOSTED_CLUSTER_NS}"-"${HOSTED_CLUSTER_NAME}
 export ASSISTED_PULLSECRET_JSON="${ASSISTED_PULLSECRET_JSON:-${PULL_SECRET_FILE}}"
 export ASSISTED_PRIVATEKEY_NAME="${ASSISTED_PRIVATEKEY_NAME:-assisted-ssh-private-key}"
 export INFRAENV_NAME=${HOSTED_CLUSTER_NAME}
@@ -46,6 +47,6 @@ if [ $_agentExist -eq 0 ]; then
 fi
 
 echo "scale nodepool replicas => $NUM_EXTRA_WORKERS"
-oc scale nodepool ${HOSTED_CLUSTER_NAME} -n clusters --replicas ${NUM_EXTRA_WORKERS}
+oc scale nodepool ${HOSTED_CLUSTER_NAME} -n "$HOSTED_CLUSTER_NS" --replicas ${NUM_EXTRA_WORKERS}
 echo "wait agent ready"
 oc wait --all=true agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE} --for=jsonpath='{.status.debugInfo.state}'=added-to-existing-cluster --timeout=45m
