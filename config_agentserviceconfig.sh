9465d72355c2594d3f6b84e13ba985b88a6d8f4b
Liangquan Li <liangli@redhat.com>
2023-09-03 14:16:51 +0800

    update config_agentserviceconfig.sh

--- config_agentserviceconfig.sh
+++ config_agentserviceconfig.sh
@@ -118,8 +118,6 @@ data:
     done)
 EOCR
 
-  python ${__dir}/set_ca_bundle.py "${WORKING_DIR}/registry/certs/registry.2.crt" "./assisted-mirror-config"
-  tee < ./assisted-mirror-config >(oc apply -f -)
 }
 
 ${__dir}/libvirt_disks.sh create
